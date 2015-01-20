bd418bb4fc0d110cdfe593bceb2a07c056dc4f5c
mifl <mifl.pc@gmail.com>
2015-01-20 21:25:55 +0100

    msm8960: use COMMON_PATH

--- msm8960.mk
+++ msm8960.mk
@@ -1,13 +1,15 @@
-DEVICE_PACKAGE_OVERLAYS := device/qcom/msm8960/overlay
+COMMON_PATH := device/pantech/msm8960-common
+
+DEVICE_PACKAGE_OVERLAYS := $(COMMON_PATH)/overlay
 
 TARGET_USES_QCOM_BSP := true
 ifeq ($(TARGET_USES_QCOM_BSP),true)
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 #media
 PRODUCT_COPY_FILES += \
-                      device/qcom/msm8960/media/media_profiles_8960.xml:system/etc/media_profiles.xml
+                      $(COMMON_PATH)/media/media_profiles_8960.xml:system/etc/media_profiles.xml
 PRODUCT_COPY_FILES += \
-                      device/qcom/msm8960/media/media_codecs_8960.xml:system/etc/media_codecs.xml
+                      $(COMMON_PATH)/media/media_codecs_8960.xml:system/etc/media_codecs.xml
 else
     TARGET_ENABLE_QC_AV_ENHANCEMENTS := false
 endif #TARGET_USES_QCOM_BSP
@@ -39,23 +41,23 @@ PRODUCT_COPY_FILES += \
 
 # Audio configuration file
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8960/audio_policy.conf:system/etc/audio_policy.conf \
+    $(COMMON_PATH)/audio_policy.conf:system/etc/audio_policy.conf \
 
 # audio UCM files
-PRODUCT_COPY_FILES += device/qcom/msm8960/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_2x_Fusion3_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3_auxpcm \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_2x_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_2x_auxpcm \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_I2S:system/etc/snd_soc_msm/snd_soc_msm_I2S \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_I2SFusion:system/etc/snd_soc_msm/snd_soc_msm_I2SFusion \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_Sitar_Sglte:system/etc/snd_soc_msm/snd_soc_msm_Sitar_Sglte \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_Sitar_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_Sitar_auxpcm \
-                      device/qcom/msm8960/snd_soc_msm/snd_soc_msm_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_auxpcm \
+PRODUCT_COPY_FILES += $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_2x_Fusion3_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3_auxpcm \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_2x_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_2x_auxpcm \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_I2S:system/etc/snd_soc_msm/snd_soc_msm_I2S \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_I2SFusion:system/etc/snd_soc_msm/snd_soc_msm_I2SFusion \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_Sitar:system/etc/snd_soc_msm/snd_soc_msm_Sitar \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_Sitar_Sglte:system/etc/snd_soc_msm/snd_soc_msm_Sitar_Sglte \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_Sitar_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_Sitar_auxpcm \
+                      $(COMMON_PATH)/snd_soc_msm/snd_soc_msm_auxpcm:system/etc/snd_soc_msm/snd_soc_msm_auxpcm \
 
 # gps/location secuity configuration file
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8960/sec_config:system/etc/sec_config
+    $(COMMON_PATH)/sec_config:system/etc/sec_config
 
 PRODUCT_PACKAGES += \
     battery_monitor \
@@ -73,9 +75,9 @@ PRODUCT_PACKAGES += fstab.qcom
 
 #wlan driver
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8960/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    device/qcom/msm8960/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
-    device/qcom/msm8960/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
+    $(COMMON_PATH)/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(COMMON_PATH)/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(COMMON_PATH)/WCNSS_qcom_wlan_nv.bin:persist/WCNSS_qcom_wlan_nv.bin
 
 PRODUCT_PACKAGES += \
     wpa_supplicant_overlay.conf \
